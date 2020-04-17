a0248dcc11d457dfe62c9c73f185e1fabc7c79be
Jung Soo Choi <chlwjdtn15@hotmail.com>
2020-04-17 06:40:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+class Main {
+  public static void main(String[] args) {
+    
+    //구구단 8 단 1000단 부터 -500단 까지
+    int i = 1;
+    int x = 8;
+
+    while(i <= 100){
+      System.out.println(x * i);
+      i++;
+
+    }
+
+    i = 0;
+
+    while(i >= -500){
+      System.out.println(x * i);
+      i--;
+    }
+
+
+    //1 부터 5까지 더하기
+    i = 1;
+    int sum = 0;
+    while ( i <= 5){
+      sum += i;
+      i++;
+    }
+    System.out.println(sum);
+  }
+}
+//해냈다! 복습완료
